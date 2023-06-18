44ee6dc1d7f8eea3643956c3f73f1cb845f1d5b7
sakibchowdhury131 <sakibchowdhury131@gmail.com>
2023-06-18 13:29:55 +0600

    exit when error occurs

--- CheckingProtocols/Rename.py
+++ CheckingProtocols/Rename.py
@@ -10,6 +10,7 @@ def rename(AUDIO_SOURCE = './dataset/audio_files',
     print(f'Renaming files in ----> {AUDIO_SOURCE}')
     fileExtension = random.choice(os.listdir(AUDIO_SOURCE)).split('.')[-1]
     with open(METADATA, 'r') as f:
+        print('I am reading metadata')
         lines = f.readlines()
 
     if not os.path.exists(TEMP_DIR):
@@ -20,7 +21,9 @@ def rename(AUDIO_SOURCE = './dataset/audio_files',
         utt = line.split('\t')[2]
         current_filename = f'{AUDIO_SOURCE}/{utt_id}.{fileExtension}'
         new_filename = f'{AUDIO_SOURCE}/{spk_id}-{utt_id}.{fileExtension}'
+        print('renaming ...')
         os.rename(current_filename, new_filename)
+        print('renamed')
 
         with open(f'{TEMP_DIR}/metadata.tsv', 'a') as f:
             f.write(f"{spk_id}-{utt_id}\t{spk_id}\t{utt}")
