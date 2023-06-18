ff763ca4bc0e3b14386c9d42b62abf88319018ab
sakibchowdhury131 <sakibchowdhury131@gmail.com>
2023-06-18 14:43:25 +0600

    m4a bug try

--- utils/m4atowav.py
+++ utils/m4atowav.py
@@ -16,5 +16,5 @@ if not os.path.exists('./temp/wavs_out'):
 
 for i in tqdm(range(totalFiles)):
     wav_file_name = './temp/wavs_out/'+m4a_files[i][0:-4] + '.wav'
-    track = AudioSegment.from_file(os.path.join(args.audio_source,m4a_files[i]),  format= 'mp3')
+    track = AudioSegment.from_file(os.path.join(args.audio_source,m4a_files[i]),  format= 'm4a')
     file_handle = track.export(wav_file_name, format='wav')
