0851dfd67e39ed7292a6192cbf30568067233c88
sakibchowdhury131 <sakibchowdhury131@gmail.com>
2023-06-18 13:36:10 +0600

    bug solved

--- CheckingProtocols/Rename.py
+++ CheckingProtocols/Rename.py
@@ -9,7 +9,6 @@ def rename(AUDIO_SOURCE = './dataset/audio_files',
     TEMP_DIR = './temp'
     print(f'Renaming files in ----> {AUDIO_SOURCE}')
     fileExtension = random.choice(os.listdir(AUDIO_SOURCE)).split('.')[-1]
-    print(METADATA)
     with open(METADATA, 'r') as f:
         print('I am reading metadata')
         lines = f.readlines()
@@ -22,9 +21,7 @@ def rename(AUDIO_SOURCE = './dataset/audio_files',
         utt = line.split('\t')[2]
         current_filename = f'{AUDIO_SOURCE}/{utt_id}.{fileExtension}'
         new_filename = f'{AUDIO_SOURCE}/{spk_id}-{utt_id}.{fileExtension}'
-        print('renaming ...')
         os.rename(current_filename, new_filename)
-        print('renamed')
 
         with open(f'{TEMP_DIR}/metadata.tsv', 'a') as f:
             f.write(f"{spk_id}-{utt_id}\t{spk_id}\t{utt}")
