4556f02d6f3fcd84b8bbabc056b5632175a7900c
sakibchowdhury131 <sakibchowdhury131@gmail.com>
2023-07-04 14:05:25 +0600

    mixed audio format support added

--- utils/convert_format_to_wav.py
+++ utils/convert_format_to_wav.py
@@ -15,7 +15,7 @@ if not os.path.exists('./temp/wavs_out'):
     os.makedirs('./temp/wavs_out')
 
 for i in tqdm(range(totalFiles)):
-    format = m4a_files[i][-3:]
-    wav_file_name = './temp/wavs_out/'+m4a_files[i][0:-4] + '.wav'
+    format = m4a_files.split('.')[-1]
+    wav_file_name = './temp/wavs_out/'+m4a_files[i].split('.')[0] + '.wav'
     track = AudioSegment.from_file(os.path.join(args.audio_source,m4a_files[i]),  format = format)
     file_handle = track.export(wav_file_name, format='wav')
